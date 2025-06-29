fff82fa47ab158467c4f0dadcb8ab21bc41cc6f7
zernet <zernetcsgo@gmail.com>
2025-06-29 23:32:13 +0200

    Add description

--- src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
+++ src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
@@ -4,6 +4,7 @@ public class Converter {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
+        System.out.println("App for measures converting.");
         System.out.println("Version 1.0");
 
         double kgs = 5;

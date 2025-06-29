f7e1f6b40e1f5104652e7edc2b3736831c689432
zernet <zernetcsgo@gmail.com>
2025-06-29 23:26:36 +0200

    Setup Version

--- src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
+++ src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
@@ -4,7 +4,7 @@ public class Converter {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {   // ← мала літера m
-        System.out.println("Converter App.");
+        System.out.println("Version 1.0");
 
         double kgs = 5;
         double pnds = 12;

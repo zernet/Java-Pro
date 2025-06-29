c3925f61747196a9b59f609256eb44534a6c6352
zernet <zernetcsgo@gmail.com>
2025-06-29 23:22:28 +0200

    Add lb→kg conversion and extended output

--- src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
+++ src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
@@ -5,12 +5,19 @@ public class Converter {
 
     public static void main(String[] args) {   // ← мала літера m
         System.out.println("Converter App.");
+
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Result is " + pounds + " pounds.");
+        double kilos  = convPoundsToKgs(pnds);
+        System.out.println("Result is " + pounds + " pounds and " + kilos + " kgs.");
     }
 
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }

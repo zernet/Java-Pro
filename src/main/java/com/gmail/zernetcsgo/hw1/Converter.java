c1dd39b31be78bd4d667412b3ec9f7adc2e4a208
zernet <zernetcsgo@gmail.com>
2025-06-29 23:25:00 +0200

    Print friendly app description

--- src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
+++ src/main/java/com/gmail/zernetcsgo/hw1/Converter.java
@@ -3,8 +3,8 @@ package com.gmail.zernetcsgo.hw1;
 public class Converter {
     private static final double CONV_K = 2.20462;
 
-    public static void main(String[] args) {   // ← мала літера m
-        System.out.println("Converter App.");
+    public static void main(String[] args) {
+        System.out.println("Version 1.0.");
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
         System.out.println("Result is " + pounds + " pounds.");

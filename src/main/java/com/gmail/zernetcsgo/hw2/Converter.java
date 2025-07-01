7eee648ce56424f2178519c5e1a9b83e7705110a
zernet <zernetcsgo@gmail.com>
2025-07-01 22:00:18 +0200

    Add Converter class

---
+++ src/main/java/com/gmail/zernetcsgo/hw2/Converter.java
@@ -0,0 +1,10 @@
+package com.gmail.zernetcsgo.hw2;
+
+public final class Converter {
+    private Converter() {}
+
+    public static void print() {
+        System.out.println("Converter v1.0.0 – перетворення °F ⇄ °C");
+        System.out.println("GitHub: https://github.com/<username>/Converter");
+    }
+}

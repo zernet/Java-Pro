6c4c227cfc776319edbc3e64c61dcd98e361d3d3
zernet <zernetcsgo@gmail.com>
2025-07-01 22:10:51 +0200

    Add FahrenheitCelsiusConverter class

--- src/main/java/com/gmail/zernetcsgo/hw2/FahrenheitCelsiusConverter.java
+++ src/main/java/com/gmail/zernetcsgo/hw2/FahrenheitCelsiusConverter.java
@@ -3,11 +3,12 @@ package com.gmail.zernetcsgo.hw2;
 public final class FahrenheitCelsiusConverter {
 
     private FahrenheitCelsiusConverter() {}
-
+    // Convert to Celsius
     public static double toCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5.0 / 9.0;
     }
 
+    // Convert to Fahrenheit
     public static double toFahrenheit(double celsius) {
         return celsius * 9.0 / 5.0 + 32;
     }
